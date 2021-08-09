97d1381bbeed6abb802768d9083ac88b68dc2d32
skepticalsaif <saifnizami.official@gmail.com>
2021-08-09 17:23:01 +0530

    updated
    Signed-off-by: skepticalsaif <saifnizami.official@gmail.com>

--- server.js
+++ server.js
@@ -14,24 +14,24 @@ const users = {}
 const PORT = process.env.PORT || 2323;
 
 io.on('connection', function (socket) {
-    console.log("Socket created :" + socket.id)
-    socket.on('msg', function (data) {
-        socket.broadcast.emit('msg', {
-            sender: users[socket.id],
-            message: data.message
-        })
-    })
-    socket.on('login', function (data) {
-        users[socket.id] = data.username
-        socket.emit('logged_in')
-    })
-    // socket.on('play', function(data) {
-    //     io.emit('play', data)
-    // })
+  console.log("Socket created :" + socket.id)
+  // socket.on('msg', function (data) {
+  //     socket.broadcast.emit('msg', {
+  //         sender: users[socket.id],
+  //         message: data.message
+  //     })
+  // })
+  // socket.on('login', function (data) {
+  //   users[socket.id] = data.username
+  //   socket.emit('logged_in')
+  //})
+  socket.on('play', function (data) {
+    io.emit('play', data)
+  })
 })
 
 app.use('/', express.static(__dirname + "/public_static"))
 
 server.listen(PORT, () => {
-    console.log(`Server started at http://localhost:${PORT}`)
+  console.log(`Server started at http://localhost:${PORT}`)
 })
